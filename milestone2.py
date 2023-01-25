323b7e20ee7c336d416bc34ec943c7af5dde6b4b
Rishan Rahman <rishanrahman@192.168.1.116>
2023-01-25 15:32:36 +0000

    Adding checks to see if input is a string and in alphabet

--- milestone2.py
+++ milestone2.py
@@ -8,3 +8,8 @@ word = random.choice(word_list)
 print(word)
 
 guess = input('Please enter 1 letter')
+
+if len(guess) == 1 and guess.isalpha() == True:
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
